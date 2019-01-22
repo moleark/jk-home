76fa8499afdc0aa919d4ac52eea4dfca082c6c9a
ligsh <system6@jkchemical.com>
2019-01-22 20:46:56 +0800

    订单预览添加class

--- src/ui/customer/tuid/contact.tsx
+++ src/ui/customer/tuid/contact.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export const contactUI = {
     content: (values: any) => {
         let { name, mobile, addressString } = values;
-        return <div>
+        return <div className="flex-grow-1">
             <b>
                 {name}
             </b>
