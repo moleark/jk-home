a1dbb82ba7ff635ead6f9997fe1ed5f9e5821a9b
ligsh <system6@jkchemical.com>
2019-02-13 10:58:51 +0800

    build 更改

--- build/service-worker.js
+++ build/service-worker.js
@@ -15,7 +15,7 @@ importScripts("./workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "./workbox-v3.6.3"});
 
 importScripts(
-  "./precache-manifest.5c4cdf3f9e478cfc8316d2b13d189fa7.js"
+  "./precache-manifest.a6d795ca6173299266bf627cb44e00bf.js"
 );
 
 workbox.clientsClaim();
