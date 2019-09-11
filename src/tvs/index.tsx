82688c6a8dd85a10b819f2440138bec364087673
Henry <liaohengyi@outlook.com>
2019-09-11 08:30:40 -0400

    tvs customer

--- src/tvs/index.tsx
+++ src/tvs/index.tsx
@@ -3,10 +3,12 @@ import { common } from "./common";
 import { warehouse } from "./warehouse";
 import { product } from "./product";
 import { order } from "./order";
+import { customer } from './customer';
 
 export const tvs:TVs = {
     common: common,
     warehouse: warehouse,
     product: product,
     order: order,
+    customer: customer,
 }
