62d8b510a47e91cb3ce5bf7c3e457bf7b2f60826
ligsh <system6@jkchemical.com>
2019-09-17 15:33:43 +0800

    Updates

--- precache-manifest.e932b609db18588ded1601432185981e.js
+++ precache-manifest.e932b609db18588ded1601432185981e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "36fb25788a3e614c3729",
+    "revision": "2954048401412b990ae8",
     "url": "./static/css/main.0a693add.chunk.css"
   },
   {
@@ -36,12 +36,12 @@ self.__precacheManifest = [
     "url": "./static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "36fb25788a3e614c3729",
-    "url": "./static/js/main.36fb2578.chunk.js"
+    "revision": "2954048401412b990ae8",
+    "url": "./static/js/main.29540484.chunk.js"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -76,7 +76,7 @@ self.__precacheManifest = [
     "url": "./static/css/2.d9b2d0c3.chunk.css"
   },
   {
-    "revision": "9adb4ede1e5a4a916adf20ca782d4748",
+    "revision": "d059618d3b9c3c152f77d5193161094f",
     "url": "./index.html"
   }
 ];
