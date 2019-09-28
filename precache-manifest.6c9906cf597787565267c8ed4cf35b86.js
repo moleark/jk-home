da54834e3e32e369afa8a98dc7ddd967142d1d02
ligsh <system6@jkchemical.com>
2019-09-28 11:41:14 +0800

    Updates

--- precache-manifest.6c9906cf597787565267c8ed4cf35b86.js
+++ precache-manifest.6c9906cf597787565267c8ed4cf35b86.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "fc8397b26e7a058c79b4",
+    "revision": "e2cdc9d1ee1ee10486da",
     "url": "./static/css/main.751e6d5e.chunk.css"
   },
   {
@@ -36,12 +36,12 @@ self.__precacheManifest = [
     "url": "./static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "fc8397b26e7a058c79b4",
-    "url": "./static/js/main.fc8397b2.chunk.js"
+    "revision": "e2cdc9d1ee1ee10486da",
+    "url": "./static/js/main.e2cdc9d1.chunk.js"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -76,7 +76,7 @@ self.__precacheManifest = [
     "url": "./static/css/3.70a324dc.chunk.css"
   },
   {
-    "revision": "bc0fc8d3cc6bdbe1dbb4fa6898314937",
+    "revision": "0d82af04f4524e1c7202d2c5b6e3ce30",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("./workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "./workbox-v3.6.3"});
 
 importScripts(
-  "./precache-manifest.79b06099c408ef79334e046c6be042b6.js"
+  "./precache-manifest.6c9906cf597787565267c8ed4cf35b86.js"
 );
 
 workbox.clientsClaim();
